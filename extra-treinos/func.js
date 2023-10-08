481009064d4634be6c3ddeec6e20790ca3b79ab3
Janderson <ffjanderson@gmail.com>
2023-10-08 17:22:34 -0300

    <treino> extra import-export vars js

---
+++ extra-treinos/func.js
@@ -0,0 +1,11 @@
+// link: https://www.geeksforgeeks.org/import-and-export-in-node-js/
+
+function add(x,y){
+    return x+y;
+}
+
+function subtract(x, y){
+    return x-y;
+}
+
+module.exports = {add}
---
+++ extra-treinos/main.js
@@ -0,0 +1,4 @@
+const f = require("./func")
+
+const result = f.add(10, 5);
+console.log('The result is: ', result);
